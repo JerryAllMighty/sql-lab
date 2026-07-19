a98e65878ca129174f820078f9c710bfcd274101
KimYoonMin <dbsalszz@naver.com>
2026-07-19 19:37:41 +0900

    [#5] refactor: 윈도우 함수 제거와 세미 조인 최적화(구체화)를 통한 성능 개선 - 임시 테이블 제거 - group by와 in 절을 활용하여 세미 조인 최적화 - Closes #5

--- 리트코드/Managers with at Least 5 Direct Reports.sql
+++ 리트코드/Managers with at Least 5 Direct Reports.sql
@@ -1,17 +1,9 @@
 # Write your MySQL query statement below
 # https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
-with cte as (
-    select
-    *
-    , row_number() over(partition by managerId order by managerId asc) as rn
-    from employee
-)
-select
-    name
+select name
 from employee
 where id in (
-    select
-        distinct managerId
-    from cte
-    where rn >= 5
+    select managerId from employee
+    group by managerId
+    having count(*) >= 5
 )
