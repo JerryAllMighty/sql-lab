e4f8586078ef558fe47cda279e18392bc9037b86
KimYoonMin <dbsalszz@naver.com>
2026-07-20 00:58:46 +0900

    [#6] refactor: 상관 서브쿼리 실행을 LEFT JOIN 기반 조인으로 개선 - 상관 서브쿼리를 부모로 참조된 id 집합과의 left join으로 바꿈

--- 리트코드/Tree Node.sql
+++ 리트코드/Tree Node.sql
@@ -1,17 +1,17 @@
+# https://leetcode.com/problems/tree-node/description/
 # Write your MySQL query statement below
+
+with parent_ids as (
+    select distinct p_id
+    from tree
+    where p_id is not null
+)
 select
     t.id
      , case
            when t.p_id is null then 'Root'
-           when t.p_id is not null and exists (
-               select 1
-               from Tree tt
-               where tt.p_id = t.id
-           ) then 'Inner'
-           when t.p_id is not null and not exists (
-               select 1
-               from Tree tt
-               where tt.p_id = t.id
-           ) then 'Leaf'
+           when pi.p_id is not null then 'Inner'
+           else 'Leaf'
     end as `type`
-from tree t;
+from tree t
+left join parent_ids pi on pi.p_id = t.id;
