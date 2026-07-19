3bbff7ad151dd38060450e6f8ae344d82e40da97
KimYoonMin <dbsalszz@naver.com>
2026-07-19 15:44:20 +0900

    [#3] refactor: with recursive 사용시 초기 멤버 부재 및 조인 단절 문제 예외처리 - 공백 정렬 - Closes #3

--- 리트코드/Consecutive Numbers.sql
+++ 리트코드/Consecutive Numbers.sql
@@ -1,10 +1,10 @@
 select distinct num as ConsecutiveNums
 from (
          select
-             num
-              , lag(num) over w as prev
-        , lead(num) over w as `next`
+            num
+            , lag(num) over w as prev
+            , lead(num) over w as `next`
          from Logs
-                  window w as (order by id)
+         window w as (order by id)
 ) as t
 where num = prev and num = `next`
