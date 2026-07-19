0f68e635bb507f305234c361fed651d56ea911da
KimYoonMin <dbsalszz@naver.com>
2026-07-19 15:37:30 +0900

    [#3] 해당 쿼리 수정 - 윈도 함수 활용

--- 리트코드/Consecutive Numbers.sql
+++ 리트코드/Consecutive Numbers.sql
@@ -1,20 +1,10 @@
-# Write your MySQL query statement below
-with recursive cte as (
-    select
-    id
-    , num
-    , 1 as ConsecutiveNums
-    from Logs
-    where id = 1
-    union all
-    select
-    l.id
-    , l.num
-    , if(l.num = c.num, ConsecutiveNums + 1, 1) as ConsecutiveNums
-    from cte c
-    inner join Logs l on l.id = c.id + 1
-)
-
 select distinct num as ConsecutiveNums
-from cte
-where ConsecutiveNums  >= 3
+from (
+         select
+             num
+              , lag(num) over w as prev
+        , lead(num) over w as `next`
+         from Logs
+                  window w as (order by id)
+) as t
+where num = prev and num = `next`
