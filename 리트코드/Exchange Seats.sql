f99e13d56273c4b4215185aaded330f7f8ea5828
KimYoonMin <dbsalszz@naver.com>
2026-07-20 20:23:36 +0900

    [#7] refactor: 반복 상관 서브쿼리를 LEAD/LAG 윈도우 함수로 대체 - 기존의 상관 서브 쿼리 대신 윈도우 함수 적용

--- 리트코드/Exchange Seats.sql
+++ 리트코드/Exchange Seats.sql
@@ -1,16 +1,9 @@
 -- https://leetcode.com/problems/exchange-seats/description/
-
 select
-    id
-     , case
-           when s.id mod 2 = 1 then
-               case
-                   when s.id < (
-                       select count(*)
-                       from seat
-                   ) then (select student from seat where id = s.id + 1)
-                   else s.student
-                   end
-           else (select student from seat where id = s.id - 1)
-    end as student
-from seat s
+    id,
+    case
+        when id mod 2 = 1 then coalesce(lead(student) over(order by id), student)
+        else lag(student) over(order by id)
+        end as student
+from seat
+order by id;
